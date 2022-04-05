e5d397ec1298b049e38f8f1c0ff60d80981228ff
Nivin <nivkurien@gmail.com>
2022-04-05 16:19:26 -0400

    adding static map

---
+++ src/components/MapDisplay.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+export function MapDisplay(props) {
+    //seting up hook
+    const [currentValue, replacementValue] = useState(props.value)
+
+
+    return (
+        <iframe title="google-map" className="map" src={props.value}></iframe>
+    )
+}
